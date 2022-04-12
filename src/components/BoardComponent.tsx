135827a4a07724163e3b3adcf9211b54a0bc6065
Иван Мингалев <nero.fan69@gmail.com>
2022-04-12 19:47:14 +0600

    build: added moving cards and checking friendly cards

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useEffect, useState } from "react";
+import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
+import CellComponent from "./CellComponent";
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (
+      selectedCell &&
+      selectedCell !== cell &&
+      selectedCell.figure?.canMove(cell)
+    ) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+      updateBoard();
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell]);
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="board">
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+            />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default BoardComponent;
---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,37 @@
+import { Colors } from "./../Colors";
+import logo from "../../assets/black-king.png";
+import { Cell } from "../Cell";
+
+export enum FigureNames {
+  FIGURE = "Фигура",
+  KING = "Король",
+  KNIGHT = "Конь",
+  PAWN = "Пешка",
+  QUEEN = "Королева",
+  ROOK = "Ладья",
+  BISHOP = "Слон",
+}
+
+export class Figure {
+  color: Colors;
+  logo: typeof logo | null;
+  cell: Cell;
+  name: FigureNames;
+  id: number;
+
+  constructor(color: Colors, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE;
+    this.id = Math.random();
+  }
+
+  canMove(target: Cell): boolean {
+    if (target.figure?.color === this.color) return false;
+    if (target.figure?.name === FigureNames.KING) return false;
+    return true;
+  }
+  moveFigure(target: Cell): void {}
+}
